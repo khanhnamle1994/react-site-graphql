8a6e2803579fad4e1acd616cdadcf3af281f1bce
James Le <le_j6@denison.edu>
2017-07-20 17:10:51 -0700

    Styles Added

---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+const sm = 768;
+
+const md = 992;
+
+const lg = 1200;
+
+const breakpoints = {
+	xs: { min: 0, max: sm - 1, },
+	sm: { min: sm, max: md - 1, },
+	md: { min: md, max: lg - 1, },
+	lg: { min: lg, max: 100000, },
+};
+
+export default {
+	white: "#fff",
+	lightgray: "#ddd",
+	gray: "#bbb",
+	blue: "#00afd7",
+	darkBlue: "#fff",
+	darkBlueLight: "#007392",
+
+	breakpoints,
+	bps: breakpoints,
+};
